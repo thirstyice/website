f5e558ed5afc7e03cf15ab9fe0ed2ad4d2c4495f
Tauran <Tauran@Taurans-MacBook.local>
2013-07-30 10:42:00 -0700

    bit of whitespace

--- resize.js
+++ resize.js
@@ -1,6 +1,7 @@
 //  Copyright (c) 2013 Tauran Wood
 //  
 //  Copying and distribution of this file, with or without modification, is permitted in any medium without royalty provided the copyright notice and this notice are preserved.
+
 //  This file is offered as-is,without warranty of any kind.
 $(document).ready(function() { resize() });
 function resize(){
