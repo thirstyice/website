3c30e6231bed8859c92081bb6f4e65b014042955
thirstyice <tauranwood@gmail.com>
2015-01-20 20:43:03 -0700

    .

--- home/ipod-clock.js
+++ home/ipod-clock.js
@@ -1,1 +1,1 @@
-"use strict";function y(){var x=setTimeout('y()',500);var a=new Date();var h=a.getHours();var m= a.getMinutes();var g=Math.floor(m/10);var l=m-(g*10);var s=a.getSeconds();var t=Math.floor(s/10);var o=s-(t*10);$("#clock").html(h+":"+g+""+l+":"+t+""+o);return;}$(document).ready(y());
+"use strict";function y(){var x=setTimeout('y()',500);var a=new Date();var h=a.getHours();var m= a.getMinutes();var g=Math.floor(m/10);var l=m-(g*10);$("#clock").html(h+":"+g+""+l);return;}$(document).ready(y());
