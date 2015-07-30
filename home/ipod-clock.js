a5fdeb69438544ef15308c261ed350e1e1d90a26
thirstyice <tauranwood@gmail.com>
2015-07-30 01:55:56 -0600

    Removed excess

--- home/ipod-clock.js
+++
@@ -1,1 +0,0 @@
-"use strict";function y(){var x=setTimeout('y()',500);var a=new Date();var h=a.getHours();var m= a.getMinutes();var g=Math.floor(m/10);var l=m-(g*10);$("#clock").html(h+":"+g+""+l);return;}$(document).ready(y());
